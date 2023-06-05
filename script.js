e784b901506f589c7e4289591a8758fa5b30bd63
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-06-05 14:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function getWindowSize() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  return { width, height };
+}
+
+function updateWindowSize() {
+  const windowSize = getWindowSize();
+  const div = document.getElementById('sizeInfo');
+  div.innerText = 'Width: ' + windowSize.width +'Height: ' + windowSize.height;
+}
+
+// Initial window size
+window.addEventListener('load', updateWindowSize);
+
+// Update window size on resize
+window.addEventListener('resize', updateWindowSize);
